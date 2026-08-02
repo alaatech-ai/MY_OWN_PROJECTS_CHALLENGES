2afb59b88be04970f1082572c3015570de199a4c
alaatech-ai <aobeid098@gmail.com>
2026-08-02 21:52:03 +0300

    Create day36.c

---
+++ c-files/C-PROJECTS/day36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int arr[3][3];
+    
+    printf("enter 9 nbs:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
