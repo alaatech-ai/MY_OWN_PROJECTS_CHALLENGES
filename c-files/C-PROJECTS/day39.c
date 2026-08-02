378a0587797e3e3f7d907f33018df90e0fd28927
alaatech-ai <aobeid098@gmail.com>
2026-08-02 21:58:12 +0300

    Create day39.c

---
+++ c-files/C-PROJECTS/day39.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[100];
+    
+    printf("enter a word:\n");
+    scanf("%s", word);
+    
+    int len = strlen(word);
+    
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", word[i]);
+    }
+    
+    printf("\n");
+    return 0;
+}
