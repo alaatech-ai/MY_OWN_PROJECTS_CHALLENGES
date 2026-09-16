1f0522ec85ca1e233262203a353910c32787de4d
ALAE_OB <aobeid098@gmail.com>
2026-09-16 18:05:48 +0300

    Add files via upload

---
+++ c-files/C-PROJECTS/day78.c
@@ -0,0 +1,21 @@
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int i;
+     for( i =digitsSize-1; i>=0; i--){
+        if(digits[i]<9){
+            digits[i]++;
+         *returnSize = digitsSize;
+            return digits;
+        }
+        else{
+            digits[i]=0;
+        }
+     } 
+     int *res = (int*)malloc(sizeof(int) * (digitsSize+1));
+     res[0]=1;
+     int i;
+     for( i=1; i<(digitsSize+1); i++){
+        res[i]=0;
+     }
+     *returnSize = digitsSize +1;
+     return res;
+}
